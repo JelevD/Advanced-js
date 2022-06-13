009d227426326929554d37e1aa12d205a7d0afd7
JelevD <jelev87@mail.bg>
2022-06-13 15:10:03 +0300

    Advanced

--- DOM/05. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
+++ DOM/05. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+  const input = document.getElementById("email");
+  input.addEventListener("change", onChange);
+
+  function onChange(ev) {
+    const pattern = /(?<name>[a-z]+)\@(?<domain>[a-z]+)\.(?<extension>[a-z]+)/g;
+
+    if (pattern.test(input.value)) {
+      ev.target.classList.remove("error");
+    } else {
+      ev.target.classList.add("error");
+    }
+  }
 }
--- regular exam/class/class.js
+++ regular exam/class/class.js
@@ -128,7 +128,6 @@ const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgari
 console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
 console.log(summerCamp.timeToPlay("Battleship", "Petar Petarson"));
 console.log(summerCamp.registerParticipant("Sara Dickinson", "child", 200));
-//console.log(summerCamp.timeToPlay("WaterBalloonFights", "Petar Petarson", "Sara Dickinson"));
 console.log(summerCamp.registerParticipant("Dimitur Kostov", "student", 300));
 console.log(summerCamp.timeToPlay("WaterBalloonFights", "Petar Petarson", "Dimitur Kostov"));
 
